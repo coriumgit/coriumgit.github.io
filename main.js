343773a6441b052dddc0d0ce899fbd0d8f238a51
coriumgit <uniomsa@gmail.com>
2020-03-30 17:40:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ import {PsychoJS} from './psychojs/js/core/PsychoJS';
 document.querySelector("#startExpBtn").addEventListener("click", main);
 
 function main() {
-    console.log("main called: "); // + String(PsychoJS.status)
+    console.log("main called: " + String(PsychoJS.status)); 
 }
