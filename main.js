b7ceb2822e4fc1a7f26d4f3fe402867d7f054e13
omsa <uniomsa@gmail.com>
2020-06-03 17:56:19 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import {PsychoJS} from './psychojs/js/core/PsychoJS';
-
-document.querySelector("#startExpBtn").addEventListener("click", main);
-
-function main() {
-    console.log("main called: " + String(PsychoJS.status)); 
-}
